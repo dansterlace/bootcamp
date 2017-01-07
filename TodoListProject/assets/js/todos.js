fa3373dc77f3a07de08486fecd03d15871fb8533
Dan Sterlace <dpsterlace@gmail.com>
2017-01-07 11:23:19 +0700

    styling and icons added, minor flash of unstyled content likely due to using fontawesome cdn

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -18,6 +18,6 @@ $("input[type='text']").keypress(function(event){
 		var todoText = $(this).val();
 		$(this).val("");
 		//add new li
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
 	}
 })
