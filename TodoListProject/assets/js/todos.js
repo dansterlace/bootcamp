255de4d698ae33bf7cec6dec984d3ce4e052f442
Dan Sterlace <dpsterlace@gmail.com>
2017-01-07 10:53:39 +0700

    adding todo and removal/completion of new todo

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,12 +1,23 @@
 // Check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-//Click on X to delete todo
-$("span").click(function(){
+// Click on X to delete todo
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+})
+
+// Adding new todo
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//extract text and clear out input
+		var todoText = $(this).val();
+		$(this).val("");
+		//add new li
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 })
